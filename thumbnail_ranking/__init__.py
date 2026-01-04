20ec524a4dc0412629beed89de6ab26f5a9e930f
leo <coder.leo.le@gmail.com>
2026-01-04 15:52:20 +0700

    fix error

--- thumbnail_ranking/__init__.py
+++ thumbnail_ranking/__init__.py
@@ -1,13 +1,9 @@
 from thumbnail_ranking.quality_filter import QualityThresholds
 from thumbnail_ranking.clip_ranker import RankedImage
-from thumbnail_ranking.pipeline import (
-    rank_candidates,
-    run,
-)
+from thumbnail_ranking.pipeline import rank_candidates
 
 __all__ = [
     "rank_candidates",
-    "run",
     "QualityThresholds",
     "RankedImage",
 ]
